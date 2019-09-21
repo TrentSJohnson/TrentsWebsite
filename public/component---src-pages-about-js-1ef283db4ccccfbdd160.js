7bc007f78fa21d0f2afba58ddaafe775cb718d86
TrentSJohnson <zookmurnig@gmail.com>
2019-09-21 14:48:17 -0400

    copied over existing files

---
+++ public/component---src-pages-about-js-1ef283db4ccccfbdd160.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{211:function(t,e,n){"use strict";n.r(e),n.d(e,"query",(function(){return u}));var o=n(2),a=(n(0),n(209));e.default=function(t){var e=t.data;return Object(o.b)(a.a,null,Object(o.b)("h1",null,"About ",e.site.siteMetadata.title),Object(o.b)("p",null,"We're the only site running on your computer dedicated to showing the best photos and videos of pandas eating lots of food."))};var u="1097489062"}}]);
+//# sourceMappingURL=component---src-pages-about-js-1ef283db4ccccfbdd160.js.map
